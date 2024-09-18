9e4380887c298a05d744adbd184b52919930a12d
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-19 02:27:20 +0300

    home fragment ui & add side navigation

---
+++ app/src/main/java/com/example/weathercast/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.weathercast
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navview: NavigationView
+    lateinit var drawerLayout: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navview = findViewById(R.id.nav_view)
+        drawerLayout = findViewById(R.id.main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val navControUer = findNavController(this, R.id.nav_host_fragment)
+        setupWithNavController(navview, navControUer)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
