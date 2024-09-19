bd2ddbe0634a4965c5c1e7794e535d12811f3855
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-19 21:48:03 +0300

    add getinstance method to weather reposatory

--- app/src/main/java/com/example/weathercast/data/WeatherReposatory.kt
+++ app/src/main/java/com/example/weathercast/data/WeatherReposatory.kt
@@ -1,4 +1,28 @@
 package com.example.weathercast.data
 
+import android.app.Application
+
 class WeatherReposatory :WeatherReposatoryInterface{
+    /*companion object {
+        @Volatile
+        private var INSTANCE: WeatherReposatory? = null
+
+        fun getRepositoryInstance(app: Application): WeatherReposatory {
+            return INSTANCE ?: synchronized(this) {
+                WeatherReposatory().also {
+                    INSTANCE = it
+                }
+            }
+        }
+    }*/
+    companion object {
+        private var instance: WeatherReposatory? = null
+        @Synchronized
+        fun getInstance(): WeatherReposatory? {
+            if (instance == null) {
+                instance = WeatherReposatory()
+            }
+            return instance
+        }
+    }
 }
