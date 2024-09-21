fc71882ccf8c886ebb95609a5bf15915eceac883
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-21 10:16:28 +0300

    create TodayWeatherDiffUtillAdapter file

---
+++ app/src/main/java/com/example/weathercast/homeweather/view/TodayWeatherDiffUtillAdapter.kt
@@ -0,0 +1,45 @@
+/*
+package com.example.weathercast.homeweather.view
+
+import android.icu.text.SimpleDateFormat
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weathercast.R
+import com.example.weathercast.data.pojo.WeatherData
+import com.example.weathercast.databinding.TodayRecycleRecycleviewItemBinding
+import java.util.Date
+
+
+class TodayWeatherDiffUtillAdapter: ListAdapter<WeatherData, TodayWeatherDiffUtillAdapter.ViewHolder> (
+    TodaWeatherDiffUtillClass()
+){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val listRowBinding:TodayRecycleRecycleviewItemBinding=
+            DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.today_recycle_recycleview_item,parent,false)
+        return ViewHolder(listRowBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val weatherData:WeatherData=getItem(position)
+        holder.listRowBinding.todayweatherData=weatherData
+    }
+    class ViewHolder(var listRowBinding: TodayRecycleRecycleviewItemBinding) : RecyclerView.ViewHolder(listRowBinding.root)
+}
+
+@BindingAdapter("android:text")
+fun setTime(view: TextView, dt_txt: String?) {
+    val sdf: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    try {
+        val date: Date = sdf.parse(dt_txt)
+        sdf.applyPattern("h a")
+        view.text = sdf.format(date)
+    } catch (e:Exception) {
+        view.text = ""
+    }
+}
+*/
