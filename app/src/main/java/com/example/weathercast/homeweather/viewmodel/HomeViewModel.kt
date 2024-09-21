25fd368157c11dbdda5ad2e3f70f8c410502e242
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-21 05:47:02 +0300

    rename weatherData and _weatherData to _forcastWeatherData and forcastWeatherData

--- app/src/main/java/com/example/weathercast/homeweather/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/weathercast/homeweather/viewmodel/HomeViewModel.kt
@@ -1,10 +1,27 @@
 package com.example.weathercast.homeweather.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.weathercast.data.pojo.ForcastWeatherData
 import com.example.weathercast.data.reposatoru.WeatherReposatoryInterface
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class HomeViewModel(private val weatherReposatory: WeatherReposatoryInterface): ViewModel() {
+    private var _forcastWeatherData = MutableLiveData<ForcastWeatherData>()
+    val forcastWeatherData: LiveData<ForcastWeatherData> = _forcastWeatherData
+
+    fun getForecastData(latitude: String, longitude: String, measurementUnit: String) {
+        Log.d("TAG", "getForecastData: viewmodel")
+        viewModelScope.launch(Dispatchers.IO) {
+            _forcastWeatherData.postValue(weatherReposatory.getForecastData(latitude, longitude,measurementUnit))
+
+        }
+    }
 }
 
 
