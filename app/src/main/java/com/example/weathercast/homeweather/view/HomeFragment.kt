a4e23b1c9f7ac833d59d85c42700810752bfae3a
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-21 07:30:24 +0300

    add lifecycleOwner for binding and weatherImage function

--- app/src/main/java/com/example/weathercast/homeweather/view/HomeFragment.kt
+++ app/src/main/java/com/example/weathercast/homeweather/view/HomeFragment.kt
@@ -1,9 +1,12 @@
 package com.example.weathercast.homeweather.view
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -34,8 +37,27 @@ class HomeFragment : Fragment() {
         weatherReposatory = WeatherReposatory.getInstance()!!
         val viewModelFactory = HomeViewModelFactory(weatherReposatory)
         homeViewModel = ViewModelProvider(this, viewModelFactory).get(HomeViewModel::class.java)
+        binding.hfvModel=homeViewModel
+        binding.lifecycleOwner = this
         homeViewModel.getForecastData("30.0444","31.2357","metric")
         homeViewModel.getCurrentData("30.0444","31.2357","metric")
-        binding.hfvModel=homeViewModel
+    }
+}
+@BindingAdapter("imageIcon", "error")
+fun weatherImage(view: ImageView, icon: String?, error: Drawable) {
+    if (!icon.isNullOrEmpty()) {
+        val resourceId = view.context.resources.getIdentifier(
+            "icon_$icon",
+            "drawable",
+            view.context.packageName
+        )
+
+        if (resourceId != 0) {
+            view.setImageResource(resourceId)
+        } else {
+            view.setImageDrawable(error)
+        }
+    } else {
+        view.setImageDrawable(error)
     }
 }
