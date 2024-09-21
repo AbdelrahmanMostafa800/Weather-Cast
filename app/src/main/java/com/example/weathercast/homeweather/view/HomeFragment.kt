d8b51773316734afde6436320c154cabccdfd654
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-21 08:30:38 +0300

    add day date and current time

--- app/src/main/java/com/example/weathercast/homeweather/view/HomeFragment.kt
+++ app/src/main/java/com/example/weathercast/homeweather/view/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -16,6 +17,10 @@ import com.example.weathercast.data.reposatoru.WeatherReposatoryInterface
 import com.example.weathercast.databinding.FragmentHomeBinding
 import com.example.weathercast.homeweather.viewmodel.HomeViewModel
 import com.example.weathercast.homeweather.viewmodel.HomeViewModelFactory
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 
 class HomeFragment : Fragment() {
     lateinit var homeViewModel: HomeViewModel
@@ -41,8 +46,26 @@ class HomeFragment : Fragment() {
         binding.lifecycleOwner = this
         homeViewModel.getForecastData("30.0444","31.2357","metric")
         homeViewModel.getCurrentData("30.0444","31.2357","metric")
+
+        val dateFormat = SimpleDateFormat("EEE, dd MMMM, h:mm a", Locale.getDefault())
+        val currentDateAndTime: String = dateFormat.format(Date())
+        binding.currentdate=currentDateAndTime
+    }
+
+
+}
+@BindingAdapter("countryCode", "cityName")
+fun setCountryAndCityName(view: TextView, countryCode: String?, cityName: String?) {
+    if (!countryCode.isNullOrEmpty() && !cityName.isNullOrEmpty()) {
+        // Convert ISO code to full country name using Locale
+        val fullCountryName = Locale("", countryCode).displayCountry
+        view.text = "$cityName/ $fullCountryName"
+    } else {
+        view.text = "N/A"
     }
 }
+
+
 @BindingAdapter("imageIcon", "error")
 fun weatherImage(view: ImageView, icon: String?, error: Drawable) {
     if (!icon.isNullOrEmpty()) {
@@ -54,8 +77,6 @@ fun weatherImage(view: ImageView, icon: String?, error: Drawable) {
 
         if (resourceId != 0) {
             view.setImageResource(resourceId)
-        } else {
-            view.setImageDrawable(error)
         }
     } else {
         view.setImageDrawable(error)
