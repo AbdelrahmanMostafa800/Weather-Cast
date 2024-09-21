79c9ec6d109f80ebfed1e5b63d5ad6b949ba2acb
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-21 05:51:35 +0300

    Databinding in HomeFragment

--- app/src/main/java/com/example/weathercast/homeweather/view/HomeFragment.kt
+++ app/src/main/java/com/example/weathercast/homeweather/view/HomeFragment.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.weathercast.R
 import com.example.weathercast.data.reposatoru.WeatherReposatory
 import com.example.weathercast.data.reposatoru.WeatherReposatoryInterface
+import com.example.weathercast.databinding.FragmentHomeBinding
 import com.example.weathercast.homeweather.viewmodel.HomeViewModel
 import com.example.weathercast.homeweather.viewmodel.HomeViewModelFactory
 
 class HomeFragment : Fragment() {
     lateinit var homeViewModel: HomeViewModel
+    lateinit var binding: FragmentHomeBinding
     private lateinit var weatherReposatory: WeatherReposatoryInterface
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,19 +25,16 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         weatherReposatory = WeatherReposatory.getInstance()!!
         val viewModelFactory = HomeViewModelFactory(weatherReposatory)
         homeViewModel = ViewModelProvider(this, viewModelFactory).get(HomeViewModel::class.java)
-        /*homeViewModel.products.observe(this, Observer { products ->
-            adapter.submitList(products)
-            recyclerView.adapter = adapter
-        })
-        homeViewModel.getAllProducts()*/
+        homeViewModel.getForecastData("30.0444","31.2357","metric")
+        binding.hfvModel=homeViewModel
     }
 }
