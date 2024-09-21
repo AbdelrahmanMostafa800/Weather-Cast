0211ae9d5ac70dee68d78ff5f5384ba194843fcd
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-21 05:38:14 +0300

    add getCurrentWeathertData method

--- app/src/main/java/com/example/weathercast/network/ApiInterface.kt
+++ app/src/main/java/com/example/weathercast/network/ApiInterface.kt
@@ -1,12 +1,23 @@
 package com.example.lab1.retrofitapi
 
 
-import com.example.weathercast.data.pojo.WeatherData
+import com.example.weathercast.data.pojo.CurrentWeatherData
+import com.example.weathercast.data.pojo.ForcastWeatherData
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiInterface {
-    @GET("forecast?lat={lat}&lon={lon} & appid = BuildConfig.OPEN_WEATHER_MAP_API_KEY")
-   suspend fun getForecastData(@Query("lat") lat: String, @Query("lon") lon: String): WeatherData
-
+    @GET("data/2.5/forecast")
+    suspend fun getForecastData(
+        @Query("lat") lat: String,
+        @Query("lon") lon: String,
+        @Query("units") units: String = "metric",
+        @Query("appid") appId: String
+    ): ForcastWeatherData
+    suspend fun getCurrentWeathertData(
+        @Query("lat") lat: String,
+        @Query("lon") lon: String,
+        @Query("units") units: String = "metric",
+        @Query("appid") appId: String
+    ): CurrentWeatherData
 }
