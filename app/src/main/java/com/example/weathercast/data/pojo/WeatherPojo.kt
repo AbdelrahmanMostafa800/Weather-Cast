fa2a4f66571d00ee49f287abdbebf0ee316c672c
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-21 05:41:34 +0300

    add CurrentWeatherData and SysCurrent classes

--- app/src/main/java/com/example/weathercast/data/pojo/WeatherPojo.kt
+++ app/src/main/java/com/example/weathercast/data/pojo/WeatherPojo.kt
@@ -1,83 +1,108 @@
 package com.example.weathercast.data.pojo
 
-
 import com.google.gson.annotations.SerializedName
 
+data class ForcastWeatherData(
+    @SerializedName("cod") val cod: String?,
+    @SerializedName("message") val message: Int?,
+    @SerializedName("cnt") val cnt: Int?,
+    @SerializedName("list") val list: List<WeatherData>,
+    @SerializedName("city") val city: City
+)
+data class CurrentWeatherData(
 
-data class Main (
-
-    @SerializedName("temp"       ) var temp      : Double? = null,
-    @SerializedName("feels_like" ) var feelsLike : Double? = null,
-    @SerializedName("temp_min"   ) var tempMin   : Double? = null,
-    @SerializedName("temp_max"   ) var tempMax   : Double? = null,
-    @SerializedName("pressure"   ) var pressure  : Int?    = null,
-    @SerializedName("sea_level"  ) var seaLevel  : Int?    = null,
-    @SerializedName("grnd_level" ) var grndLevel : Int?    = null,
-    @SerializedName("humidity"   ) var humidity  : Int?    = null,
-    @SerializedName("temp_kf"    ) var tempKf    : Double? = null
+    @SerializedName("coord"      ) var coord: Coord?             ,
+    @SerializedName("weather"    ) var weather: ArrayList<Weather>? ,
+    @SerializedName("base"       ) var base: String?          ,
+    @SerializedName("main"       ) var main: Main?              ,
+    @SerializedName("visibility" ) var visibility: Int?               ,
+    @SerializedName("wind"       ) var wind: Wind?              ,
+    @SerializedName("clouds"     ) var clouds: Clouds?  ,
+    @SerializedName("dt"         ) var dt: Int? ,
+    @SerializedName("sys"        ) var sys: SysCurrent? ,
+    @SerializedName("timezone"   ) var timezone: Int?  ,
+    @SerializedName("id"         ) var id: Int?,
+    @SerializedName("name"       ) var name: String?,
+    @SerializedName("cod"        ) var cod: Int?
 
 )
 
 
-data class Weather (
 
-    @SerializedName("id"          ) var id          : Int?    = null,
-    @SerializedName("main"        ) var main        : String? = null,
-    @SerializedName("description" ) var description : String? = null,
-    @SerializedName("icon"        ) var icon        : String? = null
+data class SysCurrent (
 
-)
-
-data class Clouds (
-
-    @SerializedName("all" ) var all : Int? = null
+    @SerializedName("type"    ) var type    : Int?    ,
+    @SerializedName("id"      ) var id      : Int?   ,
+    @SerializedName("country" ) var country : String? ,
+    @SerializedName("sunrise" ) var sunrise : Int?    ,
+    @SerializedName("sunset"  ) var sunset  : Int?
 
 )
-
-
-data class Wind (
-
-    @SerializedName("speed" ) var speed : Double? = null,
-    @SerializedName("deg"   ) var deg   : Int?    = null,
-    @SerializedName("gust"  ) var gust  : Double? = null
-
+data class WeatherData(
+    @SerializedName("dt") val dt: Int?,
+    @SerializedName("main") val main: Main,
+    @SerializedName("weather") val weather: List<Weather>,
+    @SerializedName("clouds") val clouds: Clouds,
+    @SerializedName("wind") val wind: Wind,
+    @SerializedName("visibility") val visibility: Int?,
+    @SerializedName("pop") val pop: Int?,
+    @SerializedName("sys") val sys: Sys,
+    @SerializedName("dt_txt") val dtTxt: String?
 )
 
-data class Sys (
-
-    @SerializedName("pod" ) var pod : String? = null
-
+data class Main(
+    @SerializedName("temp") val temp: Double?,
+    @SerializedName("feels_like") val feelsLike: Double?,
+    @SerializedName("temp_min") val tempMin: Double?,
+    @SerializedName("temp_max") val tempMax: Double?,
+    @SerializedName("pressure") val pressure: Int?,
+    @SerializedName("humidity") val humidity: Int?,
+    @SerializedName("sea_level") val seaLevel: Int?,
+    @SerializedName("grnd_level") val grndLevel: Int?,
+    @SerializedName("temp_kf") val tempKf: Double?
 )
 
-
-data class WeatherData (
-
-    @SerializedName("dt"         ) var dt         : Int?               = null,
-    @SerializedName("main"       ) var main       : Main?              = Main(),
-    @SerializedName("weather"    ) var weather    : ArrayList<Weather> = arrayListOf(),
-    @SerializedName("clouds"     ) var clouds     : Clouds?            = Clouds(),
-    @SerializedName("wind"       ) var wind       : Wind?              = Wind(),
-    @SerializedName("visibility" ) var visibility : Int?               = null,
-    @SerializedName("pop"        ) var pop        : Int?               = null,
-    @SerializedName("sys"        ) var sys        : Sys?               = Sys(),
-    @SerializedName("dt_txt"     ) var dtTxt      : String?            = null
-
+data class Weather(
+    @SerializedName("id") val id: Int?,
+    @SerializedName("main") val main: String?,
+    @SerializedName("description") val description: String?,
+    @SerializedName("icon") val icon: String?
 )
-data class Coord (
 
-    @SerializedName("lat" ) var lat : Double? = null,
-    @SerializedName("lon" ) var lon : Double? = null
-
+data class Clouds(
+    @SerializedName("all") val all: Int?
 )
-data class City (
 
-    @SerializedName("id"         ) var id         : Int?    = null,
-    @SerializedName("name"       ) var name       : String? = null,
-    @SerializedName("coord"      ) var coord      : Coord?  = Coord(),
-    @SerializedName("country"    ) var country    : String? = null,
-    @SerializedName("population" ) var population : Int?    = null,
-    @SerializedName("timezone"   ) var timezone   : Int?    = null,
-    @SerializedName("sunrise"    ) var sunrise    : Int?    = null,
-    @SerializedName("sunset"     ) var sunset     : Int?    = null
+data class Wind(
+    @SerializedName("speed") val speed: Double?,
+    @SerializedName("deg") val deg: Int?,
+    @SerializedName("gust") val gust: Double?
+)
 
+data class Sys(
+    @SerializedName("pod") val pod: String?
+)
+
+data class Rain(
+    @SerializedName("3h") val threeHour: Double?
+)
+
+data class Snow(
+    @SerializedName("3h") val threeHour: Double?
+)
+
+data class Coord(
+    @SerializedName("lat") val lat: Double?,
+    @SerializedName("lon") val lon: Double?
+)
+
+data class City(
+    @SerializedName("id") val id: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("coord") val coord: Coord,
+    @SerializedName("country") val country: String?,
+    @SerializedName("population") val population: Int?,
+    @SerializedName("timezone") val timezone: Int?,
+    @SerializedName("sunrise") val sunrise: Int?,
+    @SerializedName("sunset") val sunset: Int?
 )
