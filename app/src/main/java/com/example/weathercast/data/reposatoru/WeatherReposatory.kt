9391677dd255c1e2749d837c79047039e5f38ca6
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-21 05:41:01 +0300

    add getCurrentData method

--- app/src/main/java/com/example/weathercast/data/reposatoru/WeatherReposatory.kt
+++ app/src/main/java/com/example/weathercast/data/reposatoru/WeatherReposatory.kt
@@ -1,15 +1,31 @@
 package com.example.weathercast.data.reposatoru
 
-class WeatherReposatory : WeatherReposatoryInterface {
+import com.example.mvvm.network.RemoteDataSource
+import com.example.mvvm.network.RemoteDataSourceInterface
+import com.example.weathercast.data.pojo.ForcastWeatherData
+
+class WeatherReposatory(
+    private val weatherRemoteDataSource: RemoteDataSourceInterface
+) : WeatherReposatoryInterface {
 
     companion object {
         private var instance: WeatherReposatory? = null
         @Synchronized
         fun getInstance(): WeatherReposatory? {
             if (instance == null) {
-                instance = WeatherReposatory()
+                instance = WeatherReposatory(
+                    RemoteDataSource()
+                )
             }
             return instance
         }
+    }
+
+    override suspend fun getForecastData(
+        latitude: String,
+        longitude: String,
+        measurementUnit: String
+    ): ForcastWeatherData {
+       return weatherRemoteDataSource.getForecastData(latitude, longitude,measurementUnit)
     }
 }
--- app/src/main/java/com/example/weathercast/data/reposatoru/WeatherReposatoryInterface.kt
+++ app/src/main/java/com/example/weathercast/data/reposatoru/WeatherReposatoryInterface.kt
@@ -1,4 +1,7 @@
 package com.example.weathercast.data.reposatoru
 
+import com.example.weathercast.data.pojo.ForcastWeatherData
+
 interface WeatherReposatoryInterface {
+    suspend fun getForecastData(latitude: String, longitude: String, measurementUnit: String): ForcastWeatherData
 }
