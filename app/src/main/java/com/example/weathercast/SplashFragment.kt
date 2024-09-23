44b3ee95450aba345d8c2575ec16f1d4fb85c984
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-23 04:54:25 +0300

    create splash fragment

---
+++ app/src/main/java/com/example/weathercast/SplashFragment.kt
@@ -0,0 +1,43 @@
+package com.example.weathercast
+
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+
+
+class SplashFragment : Fragment() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
