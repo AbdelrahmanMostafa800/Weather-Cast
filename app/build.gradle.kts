ab7b6445e46464cb25c1415d8ca1b39f56898bec
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-23 04:49:45 +0300

    add swip fresh,SafeArgs and google play service location dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,14 @@ android {
 }
 
 dependencies {
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    //swip fresh
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
+    //SafeArgs
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+    //google play service location
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     //Retrofit
     implementation ("com.google.code.gson:gson:2.10.1")
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
