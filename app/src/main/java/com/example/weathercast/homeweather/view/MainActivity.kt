cfe51cf407cd61e34eb44d6bce0e6906bfa7d29a
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-25 03:15:32 +0300

    add aCCESS_COARSE_LOCATION & aCCESS_FINE_LOCATION so go homefragment if aCCESS_COARSE_LOCATION=true

--- app/src/main/java/com/example/weathercast/homeweather/view/MainActivity.kt
+++ app/src/main/java/com/example/weathercast/homeweather/view/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -33,6 +34,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var homeMenuItem: MenuItem
     lateinit var splashMenuItem: MenuItem
 
+    var aCCESS_FINE_LOCATION:Boolean=false
+    var aCCESS_COARSE_LOCATION:Boolean=false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,27 +54,38 @@ class MainActivity : AppCompatActivity() {
          navController = findNavController(this, R.id.nav_host_fragment)
         setupWithNavController(navview, navController)
         currentDestination = navController.currentDestination!!
-
+        Log.d("TAG", "Activity onCreate: start")
         if (checkPermission()) {
-            homeMenuItem.setVisible(true)
-            splashMenuItem.setVisible(false)
-            if (currentDestination.id == R.id.splashFragment) {
-                findNavController(this, R.id.nav_host_fragment).navigate(R.id.action_splashFragment_to_homeFragment2)
+            Log.d("TAG", "Activity onCreate:checkPermission start")
+            if (aCCESS_FINE_LOCATION){
+
+            }else if (aCCESS_COARSE_LOCATION){
+                homeMenuItem.setVisible(true)
+                splashMenuItem.setVisible(false)
+                if (currentDestination.id == R.id.splashFragment) {
+                    findNavController(this, R.id.nav_host_fragment).navigate(R.id.action_splashFragment_to_homeFragment2)
+                }
             }
+
             if (isLocationEnabled()){
+                Log.d("TAG", "Activity onCreate:isLocationEnabled if")
                 Toast.makeText(this, "Permission granted and location is enabled", Toast.LENGTH_SHORT).show()
 
 
             }else{
+                Log.d("TAG", "Activity onCreate:isLocationEnabled else")
                 Toast.makeText(this, "Please enable location", Toast.LENGTH_SHORT).show()
             }
+            Log.d("TAG", "Activity onCreate:checkPermission end")
         }else{
+            Log.d("TAG", "Activity onCreate:checkPermission else")
             homeMenuItem.setVisible(false)
             splashMenuItem.setVisible(true)
-
+            Log.d("TAG", "Activity onCreate:checkPermission else before if")
             if (currentDestination.id == R.id.homeFragment) {
                 findNavController(this, R.id.nav_host_fragment).navigate(R.id.action_homeFragment_to_splashFragment2)
             }
+            Log.d("TAG", "Activity onCreate:checkPermission else before ActivityCompat")
             ActivityCompat.requestPermissions(this,
                 arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,
                     android.Manifest.permission.ACCESS_FINE_LOCATION),
@@ -95,12 +110,24 @@ class MainActivity : AppCompatActivity() {
                         Toast.makeText(this, "Location permission denied permanently", Toast.LENGTH_SHORT).show()
                     }
                 } else {
-                    // Permission granted, proceed with the app's functionality
-                    if (currentDestination.id == R.id.splashFragment) {
-                        homeMenuItem.setVisible(true)
-                        splashMenuItem.setVisible(false)
-                        findNavController(this, R.id.nav_host_fragment).navigate(R.id.action_splashFragment_to_homeFragment2)
+                    Log.d("TAG", "onRequestPermissionsResult: Permission granted ")
+                    // Update the permission variables
+                    aCCESS_FINE_LOCATION = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    aCCESS_COARSE_LOCATION = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+                    // Now you can check the permission variables
+                    if (aCCESS_FINE_LOCATION){
+                        Log.d("TAG", "onRequestPermissionsResult: aCCESS_FINE_LOCATION")
+
+                    }else if (aCCESS_COARSE_LOCATION){
+                        Log.d("TAG", "onRequestPermissionsResult: aCCESS_COARSE_LOCATION")
+                        if (currentDestination.id == R.id.splashFragment) {
+                            homeMenuItem.setVisible(true)
+                            splashMenuItem.setVisible(false)
+                            findNavController(this, R.id.nav_host_fragment).navigate(R.id.action_splashFragment_to_homeFragment2)
+                        }
                     }
+
                 }
             }
         }
@@ -108,8 +135,15 @@ class MainActivity : AppCompatActivity() {
 
 
     fun checkPermission(): Boolean {
-        return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ){
+            aCCESS_FINE_LOCATION=true
+        }
+        if ( ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            aCCESS_COARSE_LOCATION=true
+        }
+       /* return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED*/
+        return aCCESS_FINE_LOCATION || aCCESS_COARSE_LOCATION
     }
     private fun isLocationEnabled(): Boolean {
         val locationManager: LocationManager =
