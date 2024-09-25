ddbe2046a5c9cf952a4c71bf39efecc5556e7d50
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-25 03:10:34 +0300

    edit getForecastData to return flow of ForcastWeatherData

--- app/src/main/java/com/example/weathercast/data/reposatoru/WeatherReposatoryInterface.kt
+++ app/src/main/java/com/example/weathercast/data/reposatoru/WeatherReposatoryInterface.kt
@@ -2,9 +2,10 @@ package com.example.weathercast.data.reposatoru
 
 import com.example.weathercast.data.pojo.CurrentWeatherData
 import com.example.weathercast.data.pojo.ForcastWeatherData
+import kotlinx.coroutines.flow.Flow
 
 interface WeatherReposatoryInterface {
-    suspend fun getForecastData(latitude: String, longitude: String, measurementUnit: String): ForcastWeatherData
+    suspend fun getForecastData(latitude: String, longitude: String, measurementUnit: String): Flow<ForcastWeatherData>
     suspend fun getCurrentData(
         latitude: String,
         longitude: String,
