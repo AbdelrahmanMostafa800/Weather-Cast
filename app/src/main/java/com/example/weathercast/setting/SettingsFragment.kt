168105013e7f4965a9035728584585f3444588db
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-27 06:59:14 +0300

    change toolebare text with setting

--- app/src/main/java/com/example/weathercast/setting/SettingsFragment.kt
+++ app/src/main/java/com/example/weathercast/setting/SettingsFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.weathercast.R
+import com.example.weathercast.viemodel.ToolBarTextViewModel
+import kotlinx.coroutines.launch
 
 class SettingsFragment : Fragment() {
+    private val toolBarTextViewModel: ToolBarTextViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        toolBarTextViewModel = ViewModelProvider(this).get(ToolBarTextViewModel::class.java)
+//        toolBarTextViewModel.setToolbarTitle("Setting")
     }
 
     override fun onCreateView(
@@ -18,5 +26,11 @@ class SettingsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            toolBarTextViewModel.setToolbarTitle("Setting")
+        }
     }
 }
