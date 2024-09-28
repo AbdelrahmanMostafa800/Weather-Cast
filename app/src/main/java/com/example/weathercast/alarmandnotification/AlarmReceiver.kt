16b4ca94a9b2ea666d02b3c3d7dd4c9e0286c2ce
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-09-28 06:17:19 +0300

    add AlarmReceiver of BroadcastReceiver

---
+++ app/src/main/java/com/example/weathercast/alarmandnotification/AlarmReceiver.kt
@@ -0,0 +1,18 @@
+package com.example.weathercast.alarmandnotification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        // Start the AlarmSoundService to play the alarm sound
+        val serviceIntent = Intent(context, FloatingAlarmService::class.java)
+        context.startService(serviceIntent)
+    }
+
+fun stopAlarm(context: Context) {
+    val serviceIntent = Intent(context, FloatingAlarmService::class.java)
+    context.stopService(serviceIntent)
+}
+}
